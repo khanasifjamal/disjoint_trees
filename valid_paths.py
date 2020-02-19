68ac61a33a59df02c91039c7505b68ac17a66a35
severus-tux <vishwaprakash@protonmail.com>
2020-02-20 01:15:57 +0530

    ;odifhg

--- valid_paths.py
+++ valid_paths.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.random as nr
 import sys
+import copy
 
 def to_set(str):
 	s = {int(i) for i in str}
@@ -14,6 +15,34 @@ def comp(tup1,tup2):
 	if (len(s1 & s3) >0 and len(s2 & s4)>0) or ( len(s1 & s4)>0 and len(s2 & s3)>0 ):
 		return 1
 	return 0
+
+# def gen_sets(n,k):#k is the number of points, n is the number of sets
+# 	# Returns a list[] of k-tuples() of disjoint subsets
+# 	if(n<k):
+# 		return []
+# 	elif(n==k):
+# 		return [tuple([{i+1} for i in range(n)])]
+# 	else:
+# 		pass
+
+# def stirling_partitions(n,k): # Returns a list X of tuples, st each tuple in X is a partitions of the form ({1,2},{3},{4,5})
+# 	# Base cases
+# 	if (n == 0 or k == 0 or k > n): 
+# 		return []
+# 	if (k == 1): 
+# 		s = {i+1 for i in range(n)}
+# 		return [(s,)]
+# 	if (k==n):
+# 		return [({i},) for i in range(n)]
+
+# 	# S(n+1, k) = k*S(n, k) + S(n, k-1) 
+
+# 	#case 1 : singleton {n}
+# 	prev = stirling_partitions(n,k-1)
+# 	stn = [  ]
+
+
+
 
 def main():
 	file_name = sys.argv[1]
@@ -38,5 +67,8 @@ def main():
 	print("\nSize of the matrix        : %sx%s\n"
 		"Rank of the matrix       : %s\n"
 		"Determinant of the matrix : %s\n"%(n,n,rank,det))
+
+	# lst = gen_sets(5,4)
+	# print(*lst,sep="\n")
 if __name__ == "__main__":
 	main()
